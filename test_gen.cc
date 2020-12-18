2067794c838ddccfed2c4062a69947e0bab16c4d
LIParadise <a58524andy@gmail.com>
2020-12-18 20:03:37 +0800

    PASSED test bench! Below 6.

--- test_gen.cc
+++ test_gen.cc
@@ -20,24 +20,44 @@ int main(int argc, char** argv){
     }
     int d = 0;
     for( int i = 0; i <= 62; ++i ){
-        int ddd = d;
+        int ddd = d, param_cnt = 0;
         for( int j = i+1; j <= 62; ++j, ++d ){
             cout << "    foobar f" << d << "( dummy[" << setw(2) << right << d << "], ";
             cout << input << "[" << setw(2) << right << j << "], ";
             cout << input << "[" << setw(2) << right << i << "] );\n";
         }
-        cout << "    AN64   an" << i << "( o[" << i << "], {";
-        for( int k = 0, j = i+1; k < 64; ++j, ++ddd, ++k ){
-            if( ddd < d ){
-                cout << "dummy[" << ddd << "]";
-            }else{
-                cout << "1'b1";
+
+        cout << "    AN";
+
+        if      ( d - ddd > 32 ) param_cnt = 64;
+        else if ( d - ddd > 16 ) param_cnt = 32;
+        else if ( d - ddd > 5 )  param_cnt = 16;
+        else if ( d - ddd >= 2 ) param_cnt = d-ddd;
+        else                     param_cnt = 2;
+
+        cout << param_cnt;
+        cout << "   g" << i << "( o[" << i << "], ";
+        if( param_cnt > 5 ){
+            cout << "{ ";
+        }
+        if( i != 62 ){
+            for( int k = 0, j = i+1; k < param_cnt; ++j, ++ddd, ++k ){
+                if( ddd < d ){
+                    cout << "dummy[" << ddd << "]";
+                }else{
+                    cout << "1'b1";
+                }
+                if( k != param_cnt-1 ){
+                    cout << ", ";
+                }else{
+                    if ( param_cnt > 5 ){
+                        cout << " }";
+                    }
+                    cout << " );\n";
+                }
             }
-            if( k != 64-1 ){
-                cout << ", ";
-            }else{
-                cout << " } );\n";
-            }
+        }else{  // ( i==62 )
+            cout << "vec_63_bits[62], 1'b1 );\n";
         }
     }
     return 0;
